34f7e3e579bc0c43c3b391944c884c80dd747470
Bridget Weston <jablweston@gmail.com>
2021-12-24 16:16:36 -0800

    add server

---
+++ index.js
@@ -0,0 +1,20 @@
+// IMPORTS
+var express = require('express')
+var app = express()
+var path = require('path')
+var bodyParser = require('body-parser')
+let ejs = require('ejs')
+
+// APP SETUP
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, "public")))
+app.set('view engine', 'ejs')
+
+// ROUTING
+app.get('/', function(req, res, next) {
+    res.render('index', {name: "Bridget"})
+})
+
+// START SERVER
+app.listen(3000)
