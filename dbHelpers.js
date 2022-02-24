762bd73e76431a9c1f38357c4764877fd4faba56
Bridget Weston <jablweston@gmail.com>
2022-02-24 13:36:45 -0800

    new db

--- dbHelpers.js
+++ dbHelpers.js
@@ -1,36 +1,46 @@
-const sequelize = require('./database')
-const {User} = require('./models')
-const { sections } = require('./constants/pre-techccelerator-constants')
+const sqlite3 = require('sqlite3').verbose()
 
-const getAllUsers = async function() {
-    const users = await User.findAll();
-    return users
+const db = new sqlite3.Database('./users.db', sqlite3.OPEN_READWRITE, (e) => {
+    if (e) return console.error(e.message)
+
+    console.log("connection success")
+})
+
+/* db.run(
+    `CREATE TABLE users(username, full_name, last_completed)`
+) */
+
+function addUser(username, full_name, last_completed) {
+    const q = `INSERT INTO users(username, full_name, last_completed) values(?, ?, ?)`
+
+    db.run(q, [username, full_name, last_completed], (err) => {
+        if (err) return console.error(err.message)
+
+        console.log(`New user ${full_name} created!`)
+    }) 
 }
 
-const getUser = async function(username) {
-    const user = awaitUser.findOne({ where: { username } });
-    return user
-}
+//addUser("brbrbr", "Bridget Hehe", 9)
 
-const findOrCreateUser = async function(username, name, avatar_url) {
-    const [user, created] = await User.findOrCreate({
-        where: { username },
-        defaults: {name, avatar_url, last_pt_completed: sections[0].subsections[0].key}
+function getUser(username) {
+    const q = `SELECT * FROM users WHERE username="${username}"`
+
+    console.log(q)
+
+    db.all(q, (err, rows) => {
+        if (err) return console.error(err.message)
+        console.log(rows)
     })
-    return user
 }
+getUser("brbrbr")
 
-//use when user completes a sub-section of the pre-techccelerator
-const updatePtStatus = async function(username, completed) {
-    const user = await getUser(username)
-    user.set({last_pt_completed: completed})
-    await user.save();
-}
 
-const addUser = async function(username, name, avatar_url) {
-    User.create({username, name, avatar_url}).then(() => {
-        res.send("User is inserted")
-    })
-}
+/* db.all(g, [], (err, rows) => {
+    if (err) return console.error(err.message)
+    rows.forEach(row => console.log(row))
+}) */
 
-module.exports = {findOrCreateUser}
+
+db.close((err) => {
+    if (err) return console.error(err.message)
+})
