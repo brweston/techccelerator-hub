2355b2864e514bef2ce726682e7d221946978360
Bridget Weston <jablweston@gmail.com>
2022-02-27 11:48:18 -0800

    fix stuff & styles

--- constants/sidebar.js
+++ constants/sidebar.js
@@ -1,32 +1,24 @@
 //s and ss start at 0 here
 const getBackKey = (pageKey, sections, s, ss) => {
-    let bs = s + 1
-    let bss = ss + 1
+    let bs = s
+    let bss = ss - 1
+    //assume logic in ejs that if ss and s = 0 then nav btn disabled and getBackKey will not be called
     if (ss === 0) {
-        if (s > 0) {
-            bs -= 1
-            bss = sections[bs].subSections.length
-        }
+        bs -= 1
+        bss = sections[bs].subSections.length - 1
     }
-    else {
-        bss -= 1
-    }
-    return '/' + pageKey + '/' + bs + '/' + bss
+    return '/' + pageKey + '/' + (bs + 1).toString() + '/' + (bss + 1).toString()
 }
 //s and ss start at 0 here
 const getNextKey = (pageKey, sections, s, ss) => {
-    let ns = s + 1
+    let ns = s
     let nss = ss + 1
+    //assume logic in ejs that if last s and last ss then nav btn disabled and getNextKey will not be called
     if (ss === sections[s].subSections.length - 1) {
-        if (s < sections.length - 1) {
-            ns += 1
-            nss = 1
-        }
+        ns += 1
+        nss = 0
     }
-    else {
-        nss += 1
-    }
-    return '/' + pageKey + '/' + ns + '/' + nss
+    return '/' + pageKey + '/' + (ns + 1).toString() + '/' + (nss + 1).toString()
 }
 
 module.exports = { getBackKey, getNextKey }
